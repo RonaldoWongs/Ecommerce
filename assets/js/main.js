5fa3c7700b462d0516deaf2ee3733b839b79ce71
wong <deigamerwong@gmail.com>
2023-06-10 15:31:41 -0600

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+import loader from "./components/loader.js"
+import showMenu from "./components/showMenu.js"
+import showCard from "/components/showCard.js"
+/*ocultar louder*/
+
+loader()
+
+//mostrar menu//
+showMenu()
+
+// mostrar carrito//
+showCard()
+
+
