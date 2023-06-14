063b26bfee3c24b81552eb90419923f7e5601d4c
RonaldoWongs <deigamerwong@gmail.com>
2023-06-14 14:16:46 -0600

    agregando cambios al proyecto

--- assets/js/components/loader.js
+++
@@ -1,8 +0,0 @@
-function loader () {
-    window.addEventListener("load", function () {
-        const loader = document.querySelector(".loader")
-        loader.classList.add("loader--hidden")
-    })
-}
-
-export default loader
